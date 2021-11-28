cd1fb29cbdbc6a870e06ba557cb064eb65d1ff27
jensengbg-eliza-smirnova <elizasmir@gmail.com>
2021-11-28 16:23:03 +0100

    app, configure, read.me, js, css, hooks, actions

---
+++ README.md
@@ -0,0 +1,21 @@
+**Pizza**
+e-commerce app to order pizzas
+
+**Stack tech:**
+- ReactJS + hooks
+- React Router
+- Redux + Redux thunk
+- Axios
+- Json-server
+
+
+**How to start:**
+
+*Make sure your Node js vrsion is 12.14*
+
+__backend__
+- npm install
+- npm run json-server
+
+__frontend__
+- npm start
---
+++ src/pages/Home.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { Categories, SortPopup, PizzaBlock, PizzaLoadingBlock } from '../components';
+
+import { setCategory, setSortBy } from '../redux/actions/filters';
+import { fetchPizzas } from '../redux/actions/pizzas';
+
+const categoryNames = ['Meat', 'Vegetarian', 'Grill', 'Spicy', 'Closed'];
+const sortIems = [
+  { name: 'popular', type: 'popular', order: 'desc' },
+  { name: 'price', type: 'price', order: 'desc' },
+  { name: 'name', type: 'name', order: 'asc' },
+];
+
+function Home() {
+  const dispatch = useDispatch();
+  const items = useSelector(({ pizzas }) => pizzas.items);
+  const cartItems = useSelector(({ cart }) => cart.items);
+  const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded);
+  const { category, sortBy } = useSelector(({ filters }) => filters);
+
+  React.useEffect(() => {
+    dispatch(fetchPizzas(sortBy, category));
+    // eslint-disable-next-line
+  }, [category, sortBy]);
+
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+    // eslint-disable-next-line
+  }, []);
+
+  const onSelectSortType = React.useCallback((type) => {
+    dispatch(setSortBy(type));
+    // eslint-disable-next-line
+  }, []);
+
+  const handleAddPizzaToCart = (obj) => {
+    dispatch({
+      type: 'ADD_PIZZA_CART',
+      payload: obj,
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          activeCategory={category}
+          onClickCategory={onSelectCategory}
+          items={categoryNames}
+        />
+        <SortPopup
+          activeSortType={sortBy.type}
+          items={sortIems}
+          onClickSortType={onSelectSortType}
+        />
+      </div>
+      <h2 className="content__title">All pizzas</h2>
+      <div className="content__items">
+        {isLoaded
+          ? items.map((obj) => (
+              <PizzaBlock
+                onClickAddPizza={handleAddPizzaToCart}
+                key={obj.id}
+                addedCount={cartItems[obj.id] && cartItems[obj.id].items.length}
+                {...obj}
+              />
+            ))
+          : Array(12)
+              .fill(0)
+              .map((_, index) => <PizzaLoadingBlock key={index} />)}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
